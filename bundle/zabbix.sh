9802a4a9988ea0e07a88491cdfe25ea04ef2bcfa
Martin Wimpress <code@flexion.org>
2012-02-17 11:55:20 +0000

    Fixed the zabbix bundle

--- bundle/zabbix.sh
+++ bundle/zabbix.sh
@@ -5,11 +5,11 @@
 # autotools-dev
 for DIST in lucid
 do
-    ../sideport.sh -u https://launchpad.net/ubuntu/+archive/primary/+files/autotools-dev_20110511.1.dsc -c lucid ${DIST}
+    ../sideport.sh -u https://launchpad.net/ubuntu/+archive/primary/+files/autotools-dev_20110511.1.dsc -c ${DIST}
 done
 
 # zabbix
 #for DIST in lucid
 #do
-#    ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/z/zabbix/zabbix_1.8.10-1.dsc -c lucid ${DIST}
+#    ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/z/zabbix/zabbix_1.8.10-1.dsc -c ${DIST}
 #done
