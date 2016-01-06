7d9ba20a5984ead362e28eddb1d4139f3d23cc3a
Martin Wimpress <code@flexion.org>
2016-01-06 20:58:45 +0000

    Fix mew mate updates.

--- bundle/mate.sh
+++ bundle/mate.sh
@@ -2,7 +2,7 @@
 
 function build_wily() {
     #http://wiki.mate-desktop.org/status:1.12
-    #https://packages.debian.org/source/sid/<package>
+    #https://packages.debian.org/source/sid/<package>gi
     #local PPA="${DIST}-mate"
     local PPA="crazy-mate"
     for DIST in wily
