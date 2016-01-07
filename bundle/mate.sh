228a2bf2db75fc1c6aaeddd59f62277c2ca6f801
Martin Wimpress <code@flexion.org>
2016-01-07 10:40:04 +0000

    Completed for MATE 1.12.1.

--- bundle/mate.sh
+++ bundle/mate.sh
@@ -7,7 +7,7 @@ function build_wily() {
     local PPA="crazy-mate"
     for DIST in wily
     do
-        ../sideport.sh -u http://ftp.de.debian.net/debian/pool/main/m/mate-common/mate-common_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-common/mate-common_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
         ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-desktop/mate-desktop_1.12.1-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
         ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-user-guide/mate-user-guide_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
         ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/libm/libmatekbd/libmatekbd_1.12.1-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
@@ -23,7 +23,7 @@ function build_wily() {
         ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-menus/mate-menus_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
         ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-panel/mate-panel_1.12.1-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
         ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-backgrounds/mate-backgrounds_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-themes/mate-themes_1.12.2+gtk3.18-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST} -t
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-themes/mate-themes_1.12.2+gtk3.18-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
         ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-notification-daemon/mate-notification-daemon_1.12.1-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
         ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-control-center/mate-control-center_1.12.1-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
         ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-screensaver/mate-screensaver_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
@@ -37,19 +37,19 @@ function build_wily() {
         ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/c/caja-dropbox/caja-dropbox_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
         ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/c/caja-extensions/caja-extensions_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
         ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/e/eom/eom_1.12.1-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-applets/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-indicator-applet/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-netbook/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-netspeed/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-sensors-applet/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-terminal/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-user-share/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-utils/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mozo/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/p/pluma/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/p/python-caja/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/g/gnome-main-menu/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
-        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-desktop-environment/ -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-applets/mate-applets_1.12.1-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-indicator-applet/mate-indicator-applet_1.12.1-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-netbook/mate-netbook_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-netspeed/mate-netspeed_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-sensors-applet/mate-sensors-applet_1.12.1-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-terminal/mate-terminal_1.12.1-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-user-share/mate-user-share_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-utils/mate-utils_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mozo/mozo_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/p/pluma/pluma_1.12.1-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/p/python-caja/python-caja_1.12.0-1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/g/gnome-main-menu/gnome-main-menu_1.8.0-5.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
+        ../sideport.sh -u http://ftp.de.debian.org/debian/pool/main/m/mate-desktop-environment/mate-desktop-environment_1.12.0+1.dsc -p ppa:ubuntu-mate-dev/${PPA} -c ${DIST}
     done
 }
 
